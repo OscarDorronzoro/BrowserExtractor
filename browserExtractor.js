c66b10cc03d2ae513369cb328006e18513bceb07
Oscar Dorronzoro <48965382+OscarDorronzoro@users.noreply.github.com>
2026-02-22 15:31:08 -0300

    Update browserExtractor.js

--- browserExtractor.js
+++ browserExtractor.js
@@ -1,5 +1,8 @@
-arr=[];
-for(i=0;i<document.getElementsByClassName('iHxmLe').length;i++){
-  arr.push(document.getElementsByClassName('iHxmLe')[i].firstChild.href);
+arr = [];
+iter = document.getElementsByClassName('elementor-cta__bg elementor-bg lazyloaded');
+for(i=0; i<iter.length; i++) {
+  ori = iter[i].getAttribute("data-bg-image");
+	mod = ori.split('(')[1].split(')')[0];
+	arr.push(mod);
 };
-arr.reduce((acc, cur)=>{return acc+'\n'+cur}); 
+arr.reduce((acc, cur)=>{return acc+'\n'+cur});
